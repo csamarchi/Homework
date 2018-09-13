b0ffd6835536f04fe1a77303fdf19d1212e26f08
Christine Samarchi <christinesamarchi@christines-air.lan>
2018-09-12 20:46:19 -0500

    Getting to Know You answered

--- 9-12-homework/app.js
+++ 9-12-homework/app.js
@@ -27,16 +27,50 @@
 
 //Fizz Buzz
 
-for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0 && i % 5 === 0) {
-      console.log('FizzBuzz');
-  } else {
-      if (i % 3 === 0) {
-        console.log('Fizz'); }
-      else if (i % 5 === 0) {
-        console.log('Buzz');
-      } else {
-        console.log(i);
-      }
-  }
-}
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//       console.log('FizzBuzz');
+//   } else {
+//       if (i % 3 === 0) {
+//         console.log('Fizz'); }
+//       else if (i % 5 === 0) {
+//         console.log('Buzz');
+//       } else {
+//         console.log(i);
+//       }
+//   }
+// }
+
+//Getting to Know You
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//1.
+shahzad[0] = "Gameboy";
+console.log(shahzad);
+
+//2.
+daniel[1] += 1;
+console.log(daniel);
+
+//3.
+ryan[2] = "Gotham City";
+console.log(ryan);
+
+//4.
+reuben.pop("Durham");
+reuben.push("Chicago");
+console.log(reuben);
+
+//5.
+jim.pop("All Cities");
+jim.push("Austin", "Atlanta", "Los Angeles");
+console.log(jim);
+
+//6.
+jim.splice(2, 1);
+console.log(jim);
