d034239e970a41aaa7b5982075962bb3343d5212
Christine Samarchi <christinesamarchi@christines-air.lan>
2018-09-12 20:09:24 -0500

    Excited Kittens answered

--- 9-12-homework/app.js
+++ 9-12-homework/app.js
@@ -4,8 +4,23 @@
 // }
 
 //Get Even
-for (let i = 0; i <= 200; i++) {
+// for (let i = 0; i <= 200; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
+
+let array = ["...human...why you taking pictures of me?...",
+            "...the catnip made me do it...",
+            "...why does the red dot always get away..."];
+let x = 'Love me, pet me! HSSSSSS!';
+
+for (let i = 0; i <= 20; i++) {
+  //Print this 20 times
+  console.log(x);
+
+  //Print random cat statement
   if (i % 2 === 0) {
-    console.log(i);
+    console.log(array[Math.floor(Math.random()*array.length)]);
   }
 }
