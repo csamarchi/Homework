063c3519e72ac0a7d2ff4e0939453c781508f343
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-09-14 15:51:59 -0500

    Q +A

---
+++ 9-14-homework/app.js
@@ -0,0 +1,23 @@
+//1. Variables & Datatypes
+
+//A. Q + A
+
+//1. How do we assign a value to a variable?
+//let x = 5;
+//2. How do we change the value of a variable?
+//just reference the variable inside the function
+//3.How do we assign an existing variable to a new variable?
+//you do this by calling it with a '.' key.oldVariable = 'new';
+//4.Remind me, what are declare, assign, and define?
+//Declare: declaring something exists, such as class,
+//function, or variable- you just say it exists
+//Define: define how something is implement, such as class,
+//function, or variable- you say what it actually is
+//Assign: giving your variable a value
+//5. What is pseudocoding and why should you do it?
+//pseudocoding is writing down in english sentences
+//what exactly you are telling the computer to do
+  //6. What percentage of time should be spent thinking
+  //about how you're going to solve a problem vs actually
+  //typing in code to solve it?
+//80% planning and thinking it out. 20% coding it 
