98cd03d59df55dc07db93f66a22bd71d0b06ec10
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-09-17 21:03:10 -0500

    crazyObject

--- 9-17-homework/app.js
+++ 9-17-homework/app.js
@@ -13,16 +13,55 @@
 
 //2. Take it Easy
 
-let rainbow = ['red', 'orange', 'yellow',
-               'green', 'blue', 'purple'];
+// let rainbow = ['red', 'orange', 'yellow',
+//                'green', 'blue', 'purple'];
+//
+// console.log(rainbow[4]);
+//
+// let name = {
+//   favoriteFood: 'sushi',
+//   hobby: 'eating',
+//   residence: 'austin',
+//   datatype: 'functions'
+// }
+//
+// console.log(name.hobby);
 
-console.log(rainbow[4]);
-
-let name = {
-  favoriteFood: 'sushi',
-  hobby: 'eating',
-  residence: 'austin',
-  datatype: 'functions'
+//3. Crazy Object!
+const crazyObject = {
+  taco: [
+    {
+      meat: 'steak',
+      cheese: ['panela', 'queso', 'chihuahua']
+    },
+    {
+      meat: 'chicken',
+      salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+    },
+  ],
+  larry: {
+    nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+    quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+    characters: [
+      {
+        name: "Jeff",
+        occupation: "manager"
+      },
+      {
+        name: "funkhauser",
+        occupation: "tv dude"
+      },
+      {
+        name: "susie",
+        occupation: "jeffs wife",
+        favourtieHobby: "Swearing at Larry and Jeff"
+      },
+    ]
+  }
 }
 
-console.log(name.hobby);
+console.log(crazyObject.taco[1].salsa[5]);
+console.log(crazyObject.larry.quotes[0]);
+console.log(crazyObject.larry.characters[2].favourtieHobby);
+console.log(crazyObject.larry.nicknames[1]);
+console.log(crazyObject.larry.characters[1].name);
