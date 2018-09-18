4cbd1bee0f876a3916f3cd9be200baa7b8457597
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-09-17 20:39:48 -0500

    data structures

--- 9-17-homework/app.js
+++ 9-17-homework/app.js
@@ -1,1 +1,28 @@
-console.log('hi');
+//1. Data types
+
+
+
+// A light switch that can be either on or off. Boolean
+// A user's email address. String
+// A spaceship with a hull, laser blasters, tractor beam, and warp drive. Object
+// A list of student names from our class. Array
+// A list of student names from our class, each with a location. Array that contains Objects
+// A list of student names from our class, each with a location
+//and each with a list of favorite tv shows. Array, Objects, Strings
+
+
+//2. Take it Easy
+
+let rainbow = ['red', 'orange', 'yellow',
+               'green', 'blue', 'purple'];
+
+console.log(rainbow[4]);
+
+let name = {
+  favoriteFood: 'sushi',
+  hobby: 'eating',
+  residence: 'austin',
+  datatype: 'functions'
+}
+
+console.log(name.hobby);
