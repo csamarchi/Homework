e2c9664031b2867b3dd5db235b4fbc72a5e7cd7d
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-13 17:04:22 -0500

    got index page linked to list the objects

--- first-full-stack-homework/server.js
+++ first-full-stack-homework/server.js
@@ -3,14 +3,39 @@ const app = express();
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 
+const Food = require('./models/food.js');
+
 //middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
-app.get('/', (req, res) => {
-  res.send('this is working')
-  res.render('index.ejs')
+// app.get('/food', (req, res) => {
+//   res.send(Food);
+// })
+
+app.get('/food', (req, res) => {
+  console.log(req.body, 'this is your body');
+  res.render('index.ejs', {
+    food: Food
+  });
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
