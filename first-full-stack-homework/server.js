bf8afabd18cff651e7b9ca7f2e2f94f17c136b81
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-13 15:39:15 -0500

    first commit

---
+++ first-full-stack-homework/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log('app is alive');
+})
