3175bbfe5863ab39d78552563ad95299c9e57fc4
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-16 14:55:08 -0500

    connected server with controller and browser

--- photo-app-hw/controllers/pc.js
+++ photo-app-hw/controllers/pc.js
@@ -3,3 +3,22 @@ const express = require('express');
 const router = express.Router();
 
 const Photo = require('../models/photo');
+
+//index route
+router.get('/', (req, res) => {
+  Photo.find({}, (err, foundPhoto) => {
+    res.render('photo/index.ejs')
+    photo: foundPhoto
+  })
+})
+
+//new route
+router.get('/new', (req, res) => {
+  res.render('./photo/new.ejs')
+})
+
+
+
+
+
+module.exports = router;
--- photo-app-hw/server.js
+++ photo-app-hw/server.js
@@ -5,13 +5,14 @@ const methodOverride = require('method-override');
 require('./db/db');
 
 const userController = require('./controllers/uc');
+const photoController = require('./controllers/pc')
 
 //middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
 
 app.use('/user', userController);
-
+app.use('/photo', photoController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
