f5e053320377710ff13482f27a3eede7160381bb
Christine Samarchi <christinesamarchi@Christines-MacBook-Air.local>
2018-10-17 09:15:50 -0500

    finally got new ejs to show

--- photo-app-hw/controllers/pc.js
+++ photo-app-hw/controllers/pc.js
@@ -6,11 +6,13 @@ const Photo = require('../models/photo');
 
 //index route
 router.get('/', (req, res) => {
-  Photo.find({}, (err, foundPhoto) => {
-    res.render('photo/index.ejs')
-    photo: foundPhoto
-  })
-})
+  Photo.find({}, (err, photoFound) => {
+    res.render('./photo/index.ejs', {
+      photo: photoFound
+    });
+  });
+});
+
 
 //new route
 router.get('/new', (req, res) => {
@@ -19,16 +21,18 @@ router.get('/new', (req, res) => {
 
 //post route
 router.post('/', (req, res) => {
-  Photo.create(req.body, (err, createdPhoto) => {
-      res.redirect('/photo')
+  console.log(req.body)
+  Photo.create(req.body, (err, photoFound) => {
+    res.redirect('/photo');
   });
 });
 
 //show route
 router.get('/:index', (req, res) => {
   Photo.findById(req.params.index, (err, foundPhoto) => {
-    res.render('./photo/show.ejs');
-    photo: foundPhoto
+    res.render('./photo/show.ejs', {
+      photo: foundPhoto
+    });
   });
 });
 
